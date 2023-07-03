c17ff9b642e2c61e2279b40f522ae8627674165c
NickHoo <nickhoo555@gmail.com>
2023-07-04 00:05:52 +0800

    Update deno.js

--- deno.js
+++ deno.js
@@ -3,7 +3,7 @@ import { Application } from "https://deno.land/x/oak@v10.1.0/mod.ts";
 
 const app = new Application();
 
-app.use(proxy((ctx) => new URL("https://cdn.discordapp.com"+ctx.?router.params.url)));
+app.use(proxy((ctx) => new URL("https://cdn.discordapp.com"+ctx.router.params.url)));
 //router.get("/", proxy((ctx) => new URL("https://cdn.discordapp.com"+ctx.?router.params.url)));
 
 await app.listen({ port: 80 });
