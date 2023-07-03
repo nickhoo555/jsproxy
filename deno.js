cb723364cf9bae8449cc83374838269f09bd510d
NickHoo <nickhoo555@gmail.com>
2023-07-04 01:08:27 +0800

    dont

--- deno.js
+++ deno.js
@@ -1,9 +1,25 @@
 import { proxy } from "https://deno.land/x/oak_http_proxy@2.1.0/mod.ts";
-import { Application } from "https://deno.land/x/oak@v10.1.0/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak@v10.1.0/mod.ts";
+
 
 const app = new Application();
+const router = new Router();
+// app.use(proxy((ctx) => {
+//     console.log(ctx);
+//     return new URL("https://cdn.discordapp.com"+ctx.router.params.url)
+// }));
 
-app.use(proxy((ctx) => new URL("https://cdn.discordapp.com"+ctx.router.params.url)));
-//router.get("/", proxy((ctx) => new URL("https://cdn.discordapp.com"+ctx.?router.params.url)));
+router.get("/", proxy((ctx) => {
+    console.log(ctx);
+    return new URL(ctx.request.url.searchParams.get("url"))
+}));
 
+/* router.get("/", async (ctx)=> {
+    console.log(ctx.request.url.searchParams.get("url"));
+    return fetch(`https://example.com`);
+  }); */
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 await app.listen({ port: 80 });
+
